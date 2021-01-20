2313fc7f6baef00c09fa45382ac2e6bf436b9077
DarthHamza <hamzamakia@gmail.com>
2021-01-20 13:09:54 +0300

    Using jsx and styling basics

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+const style = {
+  display: "block",
+  marginLeft: "auto",
+  marginRight: "auto",
+  width: "100%",
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Vader's Mechanical Keyboards</h1>
+      <p className="App-link">
+        We've got all the parts you need to either build or customize your
+        keyboard
+      </p>
+      <img
+        style={style}
+        src="https://external-preview.redd.it/ieV4VoFLENgOJ7S-CBjQXUODx2xEx9rFb7VtIV0qMh0.jpg?auto=webp&s=ffcb1a82ed1cfb5001a8b30f17fc896bfb41a3f1"
+      />
     </div>
   );
 }
